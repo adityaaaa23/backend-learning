04bd9f959ded0ec4ba9000bffdaf9b915bd9a749
adityaaaa23 <ap23092005@gmail.com>
2026-07-23 20:59:51 +0530

    virtual properties

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,60 +1,67 @@
 const mongoose = require('mongoose');
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true,
-  },
-  duration: {
-    type: Number,
-    required: [true, 'we should know the duration of thr trip'],
-  },
-  maxGroupSize: {
-    type: Number,
-    required: [
-      true,
-      'number of people who can take part in the tour simontnously',
-    ],
-  },
-  difficulty: {
-    type: String,
-    required: [true, 'how difficult is the treak'],
-  },
-  ratingsAverage: {
-    type: Number,
-    default: 3.5,
-  },
+const tourSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'A tour must have a name'],
+      unique: true,
+    },
+    duration: {
+      type: Number,
+      required: [true, 'we should know the duration of thr trip'],
+    },
+    maxGroupSize: {
+      type: Number,
+      required: [
+        true,
+        'number of people who can take part in the tour simontnously',
+      ],
+    },
+    difficulty: {
+      type: String,
+      required: [true, 'how difficult is the treak'],
+    },
+    ratingsAverage: {
+      type: Number,
+      default: 3.5,
+    },
 
-  ratingsQuantity: {
-    type: Number,
-    default: 0,
+    ratingsQuantity: {
+      type: Number,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: true,
+      default: 1000,
+    },
+    priceDiscount: Number,
+    summary: {
+      type: String,
+      trim: true,
+      required: [true, 'Summery is essential'],
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    imageCover: {
+      type: String,
+      required: [true, 'image is also required'],
+    },
+    images: [String],
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+    },
+    startDates: [Date],
   },
-  price: {
-    type: Number,
-    required: true,
-    default: 1000,
-  },
-  priceDiscount: Number,
-  summary: {
-    type: String,
-    trim: true,
-    required: [true, 'Summery is essential'],
-  },
-  description: {
-    type: String,
-    trim: true,
-  },
-  imageCover: {
-    type: String,
-    required: [true, 'image is also required'],
-  },
-  images: [String],
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-  },
-  startDates: [Date],
+  { toJSON: { virtuals: true }, toObject: { virtuals: true } },
+);
+tourSchema.virtual('durationWeeks').get(function () {
+  //these are virtual proerties which can be derived from other properties rather than expilcity storing them bhut sense nai banata  prr inko queries me use nai krr skte
+  return this.duration / 7;
 });
 
 const tour = mongoose.model('Tour', tourSchema);
